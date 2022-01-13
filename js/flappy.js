bd92adc6aeb52f7c2571d4ff72a0787b54078611
Michael Medina <michael.cunha97@hotmail.com>
2022-01-13 09:49:36 -0300

    Adding the class to build one pipe for the game

--- js/flappy.js
+++ js/flappy.js
@@ -1,4 +1,20 @@
-function newElement(tag, classe) {
-  const element = document.createElement(tag, classe);
+function newElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
   return element;
 }
+
+function Barriers(reverse = false) {
+  this.element = newElement("div", "barrier");
+  console.log(this.element);
+  const pipe = newElement("div", "pipe");
+  const border = newElement("div", "border");
+  // this.element.appendChild(reverse ? pipe : border);
+  // this.element.appendChild(reverse ? border : pipe);
+  this.element.appendChild(reverse ? border : pipe);
+  this.element.appendChild(reverse ? pipe : border);
+}
+
+const b = new Barriers(false);
+console.log(b.element);
+document.querySelector("[flappy-board]").appendChild(b.element);
