07db430fd31b29d9676f30552f240a164e50a8ed
Michael Medina <michael.cunha97@hotmail.com>
2022-01-14 10:04:29 -0300

    First of all i changed the unit of measure used in the barrier, using now pixel instead of porcentage, because i'm using flexbox as container father, his son there is no a height or width, as porcentage use his father as parameter, porcentage, didn't work;
    I also changed some css property and added a container called parBarries where i'm going to draw the hieght and the gap between the top and bottom barrier

--- js/flappy.js
+++ js/flappy.js
@@ -4,7 +4,7 @@ function newElement(tag, className) {
   return element;
 }
 
-function Barriers(reverse = false) {
+function Barrier(reverse = false) {
   this.element = newElement("div", "barrier");
   console.log(this.element);
   const pipe = newElement("div", "pipe");
@@ -13,8 +13,36 @@ function Barriers(reverse = false) {
   // this.element.appendChild(reverse ? border : pipe);
   this.element.appendChild(reverse ? border : pipe);
   this.element.appendChild(reverse ? pipe : border);
+
+  this.setHeight = (altura) => {
+    pipe.style.height = `${altura}px`;
+  };
 }
 
-const b = new Barriers(false);
-console.log(b.element);
+// const b = new Barrier(false);
+// console.log(b.element);
+// b.setAltura(50);
+// document.querySelector("[flappy-board]").appendChild(b.element);
+
+function PairBarriers() {
+  this.element = newElement("div", "pair-barriers");
+  const top = new Barrier(false);
+  const bottom = new Barrier(true);
+  this.element.appendChild(top.element);
+  this.element.appendChild(bottom.element);
+
+  this.drawHeight = (height, gap) => {
+    const topHieght = Math.random() * (height - gap);
+    const bottomHieght = height - gap - topHieght;
+    console.log(`TopHeight:${topHieght}`);
+    console.log(`BottomHeight:${bottomHieght}`);
+
+    top.setHeight(topHieght);
+    bottom.setHeight(bottomHieght);
+  };
+
+  this.drawHeight(600, 200);
+}
+
+const b = new PairBarriers();
 document.querySelector("[flappy-board]").appendChild(b.element);
