1389ecb5633989f6ecb9f83e046fb7c49eb1259d
Michael Medina <michael.cunha97@hotmail.com>
2022-01-14 13:38:30 -0300

    Adding the loop of bars, but they are still bugged in regard of width

--- js/flappy.js
+++ js/flappy.js
@@ -24,25 +24,70 @@ function Barrier(reverse = false) {
 // b.setAltura(50);
 // document.querySelector("[flappy-board]").appendChild(b.element);
 
-function PairBarriers() {
+function PairBarriers(height, gap, x) {
   this.element = newElement("div", "pair-barriers");
   const top = new Barrier(false);
   const bottom = new Barrier(true);
   this.element.appendChild(top.element);
   this.element.appendChild(bottom.element);
 
-  this.drawHeight = (height, gap) => {
+  this.drawHeight = () => {
     const topHieght = Math.random() * (height - gap);
     const bottomHieght = height - gap - topHieght;
-    console.log(`TopHeight:${topHieght}`);
-    console.log(`BottomHeight:${bottomHieght}`);
 
     top.setHeight(topHieght);
     bottom.setHeight(bottomHieght);
   };
 
-  this.drawHeight(600, 200);
+  this.getX = () => parseInt(this.element.style.left.split("px")[0]);
+
+  this.setX = (x) => (this.element.style.left = `${x}px`);
+
+  this.getWidth = () => this.element.clientWidth;
+
+  this.drawHeight();
+  console.log("GetX dentro PairBarriers" + this.getX());
+  this.setX(x);
+}
+// const b = new PairBarriers(600, 200, 200);
+// document.querySelector("[flappy-board]").appendChild(b.element);
+
+function Barriers(height, gap, spaceBetween, width, sumPoint) {
+  this.pairs = [
+    new PairBarriers(height, gap, width),
+    new PairBarriers(height, gap, width + spaceBetween),
+    // new PairBarriers(height, gap, width + spaceBetween * 2),
+    // new PairBarriers(height, gap, width + spaceBetween * 3),
+  ];
+
+  let displacement = 3;
+  this.animar = () => {
+    this.pairs.forEach((pair, index) => {
+      pair.setX(pair.getX() - displacement);
+
+      if (pair.getX() < -pair.getWidth()) {
+        console.log(
+          `Barra${index}:${pair.getX()}; NovoX:${
+            pair.getX() + spaceBetween * this.pairs.length
+          }; spaceBetween:${spaceBetween}; length:${this.pairs.length};`
+        );
+        pair.setX(pair.getX() + spaceBetween * this.pairs.length);
+        pair.drawHeight();
+      }
+
+      const halfway = width / 2;
+      const crossedHalfway =
+        pair.getX() + displacement >= halfway && pair.getX() < halfway;
+
+      // if (crossedHalfway) sumPoint();
+    });
+  };
 }
 
-const b = new PairBarriers();
-document.querySelector("[flappy-board]").appendChild(b.element);
+const barriers = new Barriers(550, 200, 200, 600);
+const gameZone = document.querySelector("[flappy-board]");
+
+barriers.pairs.forEach((bar) => gameZone.appendChild(bar.element));
+setInterval(() => {
+  barriers.animar();
+}, 20);
